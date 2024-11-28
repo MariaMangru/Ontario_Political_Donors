b3d997d595c2a40831793b214749337317ed901c
MariaMangru <94894368+MariaMangru@users.noreply.github.com>
2024-11-28 11:22:14 -0500

    renamed political parties

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -11,7 +11,7 @@ library(lubridate)
 library(readr)
 
 #### Clean data ####
-raw_data <- read_csv("data/01-raw_data/raw_data.csv")
+raw_data <- read_csv("../data/01-raw_data/raw_data.csv")
 
 
 # Filter for regions 'Federal' or 'Ontario'
@@ -24,6 +24,19 @@ donations_filtered$donation_date <- as.Date(donations_filtered$donation_date)
 
 # Convert donation_year to numeric
 donations_filtered$donation_year <- as.numeric(donations_filtered$donation_year)
+
+
+# Rename political_party entries for Ontario region
+donations_filtered <- donations_filtered %>%
+  mutate(
+    political_party = case_when(
+      region == "Ontario" & political_party %in% c("Ontario Liberal Party", "Liberal Party") ~ "Liberal Party of Ontario",
+      region == "Ontario" & political_party == "New Democratic Party" ~ "New Democratic Party of Ontario",
+      region == "Ontario" & political_party == "Bloc Québécois" ~ "Bloc Quebecois",
+      region == "Federal" & political_party == "Bloc Québécois" ~ "Bloc Quebecois",  # Example for Federal region
+      TRUE ~ political_party
+    )
+  )
 
 # Federal Government Parties in Power from 2011 to 2024
 federal_government <- data.frame(
@@ -57,4 +70,4 @@ donations_filtered <- donations_filtered %>%
 
 
 #### Save data ####
-write_csv(donations_filtered, "data/02-analysis_data/analysis_data.csv")
+write_csv(donations_filtered, "../data/02-analysis_data/analysis_data.csv")

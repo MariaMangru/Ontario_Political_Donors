2f72b73a19f459095a7df92c9151e88196ab8e3e
MariaMangru <94894368+MariaMangru@users.noreply.github.com>
2024-11-29 13:20:15 -0500

    update model

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -1,11 +1,9 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models
+# Author: Maria Mangru
+# Date: 25 November 2024
+# Contact: maria.mangru@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
@@ -13,25 +11,53 @@ library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_csv("../data/02-analysis_data/analysis_data.csv")
 
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
+# Filter data for Ontario region
+ontario_data <- analysis_data %>% filter(region == "Ontario")
+
+# Define binary variables
+ontario_data <- ontario_data %>%
+  mutate(
+    In_Power = ifelse(recipient_in_power, 1, 0)
   )
 
+# Aggregate total donations per party per year in Ontario
+total_donations_ontario <- ontario_data %>%
+  group_by(political_party, donation_year) %>%
+  summarize(
+    Total_Donations = sum(amount, na.rm = TRUE),
+    In_Power = first(In_Power)
+  ) %>%
+  ungroup()
 
-#### Save model ####
-saveRDS(
-  first_model,
-  file = "models/first_model.rds"
-)
+# Run Regression for Ontario
+model_ontario <- lm(Total_Donations ~ In_Power, data = total_donations_ontario)
+summary(model_ontario)
+
+
+# Filter data for Federal region
+federal_data <- analysis_data %>% filter(region == "Federal")
+
+# Define binary variables
+federal_data <- federal_data %>%
+  mutate(
+    In_Power = ifelse(recipient_in_power, 1, 0)
+  )
+
+# Aggregate total donations per party per year at Federal level
+total_donations_federal <- federal_data %>%
+  group_by(political_party, donation_year) %>%
+  summarize(
+    Total_Donations = sum(amount, na.rm = TRUE),
+    In_Power = first(In_Power)
+  ) %>%
+  ungroup()
+
+# Run Regression for Federal
+model_federal <- lm(Total_Donations ~ In_Power, data = total_donations_federal)
+summary(model_federal)
+
+
 
 
